b20921370ca07a167e3725cde22bc6dffd641fd1
Taha Ghoneim <Taha.Ghoneim11@gmail.com>
2024-04-26 01:33:03 +0300

    book entity

--- src/main/java/com/ghoneim/development/library/management/system/entity/Book.java
+++ src/main/java/com/ghoneim/development/library/management/system/entity/Book.java
@@ -1,2 +1,19 @@
-package com.ghoneim.development.library.management.system.entity;public class Book {
+package com.ghoneim.development.library.management.system.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
+public class Book {
+
+//    Book: Includes attributes like ID, title, author, publication year, ISBN, etc.
+    private long id;
+    private String title;
+    private String author;
+    private LocalDate publicationYear;
+    private String ISBN;
+
 }
