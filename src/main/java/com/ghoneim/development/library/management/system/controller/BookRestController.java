ae797d3f03677cbc1d6960fbae1455c21505bfba
Taha Ghoneim <Taha.Ghoneim11@gmail.com>
2024-05-12 19:42:23 +0300

    book crud opreations

--- src/main/java/com/ghoneim/development/library/management/system/controller/BookRestController.java
+++ src/main/java/com/ghoneim/development/library/management/system/controller/BookRestController.java
@@ -2,14 +2,10 @@ package com.ghoneim.development.library.management.system.controller;
 
 import com.ghoneim.development.library.management.system.entity.Book;
 import com.ghoneim.development.library.management.system.service.BookService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +20,24 @@ public class BookRestController {
         return new ResponseEntity<>(bookService.getBooks(), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable(name = "id") long id){
+        return new ResponseEntity<>(bookService.getBookById(id), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Book> saveBook(@RequestBody Book book) {
+        return new ResponseEntity<>(bookService.saveBook(book), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable long id, @RequestBody Book book) {
+        return new ResponseEntity<>(bookService.updateBook(id, book), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteBook(@PathVariable long id) {
+        return new ResponseEntity<>(bookService.deleteBook(id), HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/ghoneim/development/library/management/system/service/BookService.java
+++ src/main/java/com/ghoneim/development/library/management/system/service/BookService.java
@@ -2,11 +2,9 @@ package com.ghoneim.development.library.management.system.service;
 
 import com.ghoneim.development.library.management.system.entity.Book;
 import com.ghoneim.development.library.management.system.repository.BookRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,4 +17,23 @@ public class BookService {
         return bookRepository.findAll();
     }
 
+    public Book getBookById(long id) {
+        return bookRepository.findById(id).isPresent()? bookRepository.findById(id).get():null;
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(long id, Book book) {
+        if(bookRepository.findById(id).isPresent()) {
+            book.setId(id);
+        }
+        return bookRepository.save(book);
+    }
+
+    public boolean deleteBook(long id) {
+        bookRepository.deleteById(id);
+        return true;
+    }
 }
